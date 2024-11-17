5c40f2c5338a1e72c64164b93947e4f0e52fc5a8
Nico <nico.vbiljon@gmail.com>
2024-11-17 10:49:37 +0200

    Update Songs and Guardians Object

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ const songs = [
     { title: "I Want You Back", artist: "The Jackson 5", genre: "Pop" },
     { title: "Spirit in the Sky", artist: "Norman Greenbaum", genre: "Rock" },
     { title: "Cherry Bomb", artist: "The Runaways", genre: "Rock" },
-    { title: "Escape (The Piña Colada Song)", artist: "Rupert Holmes", genre: "Pop" },
+    {
+        title: "Escape (The Piña Colada Song)",
+        artist: "Rupert Holmes",
+        genre: "Pop",
+    },
     { title: "O-O-H Child", artist: "The Five Stairsteps", genre: "R&B" },
-    { title: "Ain't No Mountain High Enough", artist: "Marvin Gaye & Tammi Terrell", genre: "R&B" },
+    {
+        title: "Ain't No Mountain High Enough",
+        artist: "Marvin Gaye & Tammi Terrell",
+        genre: "R&B",
+    },
     { title: "Come and Get Your Love", artist: "Redbone", genre: "Rock" },
     { title: "I'm Not in Love", artist: "10cc", genre: "Pop" },
-    { title: "Fooled Around and Fell in Love", artist: "Elvin Bishop", genre: "Rock" },
+    {
+        title: "Fooled Around and Fell in Love",
+        artist: "Elvin Bishop",
+        genre: "Rock",
+    },
+    { title: "November Rain", artist: "Guns N' Roses", genre: "Rock" },
+    { title: "Love Bites", artist: "Def Leppard", genre: "Rock" },
     // Feel free to add even more songs
 ];
 
-
 // Object containing each Guardian's preferred genre
 const guardians = {
-    "Star-Lord": "Rock",
-    "Gamora": "Pop",
+    name: "Star-Lord",
+    genre: "Rock",
+    name: "Gamora",
+    genre: "Pop",
+    name: "Drax",
+    genre: "R&B",
     // Add preferences for Drax, Rocket, and Groot
 };
 
@@ -30,5 +47,3 @@ function generatePlaylist(guardians, songs) {
 
 // Call generatePlaylist and display the playlists for each Guardian
 generatePlaylist(guardians, songs);
-
-
